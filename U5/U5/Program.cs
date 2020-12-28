69aa4aaa1f45dcfa26fb5efc4e824ddf34d4dc85
Iago <iago.gc88@gmail.com>
2020-12-28 23:20:54 +0100

    E1 final

---
+++ U5/U5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U5 {
+    class Program {
+        static void Main(string[] args) {
+            E1 ex1 = new E1();
+            ex1.init();
+
+            Console.ReadKey();
+        }
+    }
+}
