736504e1197958ca929a6f4f0f07629cd976c5f7
Iago <iago.gc88@gmail.com>
2020-12-29 10:05:25 +0100

    E10
    Array sum.

---
+++ E11/E11.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U5 {
+    class E11 {
+        public void init() {
+            int[] array = { 2, 34, 22, 4, 43, 82, 17, 90, 9, 13 };
+
+            Console.WriteLine("La suma de los elementos de mi array es: {0}.", array.Sum());
+        }        
+    }
+}
